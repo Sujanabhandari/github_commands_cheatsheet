ab77e3a3244768cb5231dc116184005bd2a52bf5
Sujana Bhandari <sujana6589@gmail.com>
2022-09-12 13:27:43 +0200

    Add git amendcommand and do correction and again

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
      <h1>Starting form the beginning</h1>
      <h1>Starting form the beginning2</h1>
      <h1>Starting form the beginning3</h1>
-     <h1>Complete the journey</h1>
+     <h1>Complete the journey</h1> 
+     <h1>GIT Amend command</h1>
     </div>
   );
 }
