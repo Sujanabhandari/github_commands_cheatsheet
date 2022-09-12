9e03d9a36e74b807fd1465288e0421f87e978193
Sujana Bhandari <sujana6589@gmail.com>
2022-09-12 13:04:24 +0200

    Complete the journey with squash

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="App">
      <h1>Starting form the beginning</h1>
+     <h1>Starting form the beginning2</h1>
+     <h1>Starting form the beginning3</h1>
+     <h1>Complete the journey</h1>
     </div>
   );
 }
