796faaea61fd489aaf36710cee8bbf8c7bdc4cb6
Sujana Bhandari <sujana6589@gmail.com>
2022-09-12 11:19:53 +0200

    Squash example with rebase example

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
      <h3>GIT Stash</h3>
      <p>THis is information</p>
      <p>THis is more information</p>
+     <p>THis is example of squash</p>
+     <p>THis is more example of squash command</p>
+     <p>THis is more example of squash command with rebase command</p>
     </div>
   );
 }
