f24d447f7b54e90f2f69bd342c6299f5109b7e8b
Sujana Bhandari <sujana6589@gmail.com>
2022-09-12 13:03:35 +0200

    Starting from begining

--- src/App.js
+++ src/App.js
@@ -4,13 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h3>GIT Stash</h3>
-     <p>THis is information</p>
-     <p>THis is more information</p>
-     <p>THis is example of squash</p>
-     <p>THis is more example of squash command</p>
-     <p>THis is more example of squash command with rebase command</p>
-      <p>THis is more example of Stash</p>
+     <h1>Starting form the beginning</h1>
     </div>
   );
 }
